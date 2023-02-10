7b003139cf175b64c70282b27b60aa8a62f4acba
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-10 16:28:27 +0300

    Add switch task

---
+++ Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели: ");
+int weekNumber = Convert.ToInt32(Console.ReadLine());
+string nameOfTheDay;
+
+switch (weekNumber)
+{
+  case 1:
+    nameOfTheDay = "Monday";
+    break;
+  case 2:
+    nameOfTheDay = "Tuesday";
+    break;
+  case 3:
+    nameOfTheDay = "Wednesday";
+    break;
+  case 4:
+    nameOfTheDay = "Thursday";
+    break;
+  case 5:
+    nameOfTheDay = "Friday";
+    break;
+  case 6:
+    nameOfTheDay = "Saturday";
+    break;
+  case 7:
+    nameOfTheDay = "Sunday";
+    break;
+  default:
+    Console.WriteLine("В неделе только семь дней");
+    break;
+}
