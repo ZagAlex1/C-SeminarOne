ef712d767c9ede73fb111f1033a37a196c5912d5
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-10 18:22:43 +0300

    Add LustNumber

---
+++ LustNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите нужное число ");
+while(true) {
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 10 == 0 || number / 100 > 9)
+{
+  Console.WriteLine("Введите другое число");
+}
+else
+{
+  number = number % 10;
+  Console.WriteLine(number);
+  break;
+}
+}
