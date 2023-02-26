99eb38b5da6202fa21e89cd32a8bbc862a49cf5a
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-26 21:01:03 +0300

    Add task Array12Numbers

---
+++ Array12Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int negative = 0;
+int positive = 0;
+
+int[] SetArray()
+{
+  int[] array = new int[12];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(-9, 10);
+    System.Console.Write(array[i] + " ");
+  }
+  System.Console.WriteLine();
+  return array;
+}
+
+void NegOrPos(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+      positive += array[i];
+    else
+      negative += array[i];
+  }
+  System.Console.WriteLine($"Сумма положительных : {positive} Сумма отрицательных : {negative}");
+}
+
+NegOrPos(SetArray());
