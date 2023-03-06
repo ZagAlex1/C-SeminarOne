862adea43687c77099ee55afe1e42324810ac9df
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-03-06 18:24:44 +0300

    Add task DoubleArrayMPlusN

---
+++ DoubleArrayMPlusN/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент массива находится по формуле:
+// Amn = m + n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+
+int[,] array = FillArray(3, 4);
+ReadArray(array);
+
+int[,] FillArray(int row, int column)
+{
+  int[,] array = new int[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      array[i, j] = i + j;
+    }
+  }
+  return array;
+}
+void ReadArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      System.Console.Write(array[i, j] + " ");
+    }
+    System.Console.WriteLine();
+  }
+}
